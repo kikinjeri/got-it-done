ba88779089d347568c59c81d291613ae16869f6d
Kiki <182134344+kikinjeri@users.noreply.github.com>
2025-04-16 15:24:07 -0400

    Add files via upload
    added assets folder to fix css not displaying

---
+++ assets/js/homepage.js
@@ -0,0 +1,23 @@
+var userFormE1 = document.querySelector('#user-form');
+var nameInputE1 =document.querySelector('#username');
+
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    console.log(response);
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
+};
+
+
+
+var formSubmitHandler = function(event){
+    event.preventDefault();
+    console.log(event);
+};
+userFormE1.addEventListener('submit', formSubmitHandler);
